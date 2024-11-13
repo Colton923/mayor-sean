848a35e6a0405bc45afc13e01077f080670c9920
Colton <colton923@gmail.com>
2024-11-13 01:43:42 -0600

    contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="flex flex-col items-center space-y-4">
       <h1>Contact</h1>
       <p>Reach out to us!</p>
-      <ContactForm />
+      <ContactForm c={"black"} />
     </div>
   );
 }
