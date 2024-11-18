9c4f331680bd9faf96fac36a3e28325b973c8b36
Colton <colton923@gmail.com>
2024-11-18 02:15:24 -0600

    i believe recipes/[...recipe] works now

--- app/recipes/[...recipe]/page.tsx
+++ app/recipes/[...recipe]/page.tsx
@@ -1,22 +1,22 @@
 import { type SanityDocument } from "next-sanity";
 import { client } from "@/sanity/client";
 import RecipeCard from "@/components/RecipeCard/RecipeCard";
-// import RecipeCard from "@/components/RecipeCard/RecipeCard";
+import TRecipe from "@/types/TRecipe";
 
 const RECIPE_QUERY = (slug: string) => {
-  return `*[_type == "recipe" && slug.current == ${slug}][0]`;
+  return `*[_type == "recipe" && slug.current == "${slug}"][0]`;
 };
 
 const options = { next: { revalidate: 30 } };
 
-export default async function RecipePage({
-  params,
-}: {
+type RecipePageProps = {
   params: { slug: string };
-}) {
-  const recipe = await client.fetch<SanityDocument>(
+};
+
+export default async function RecipePage({ params }: RecipePageProps) {
+  const recipe = await client.fetch<SanityDocument<TRecipe>>(
     RECIPE_QUERY(params.slug),
-    params,
+    {},
     options
   );
 
@@ -30,7 +30,7 @@ export default async function RecipePage({
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
-      <RecipeCard recipe={recipe as any} />
+      <RecipeCard recipe={recipe} />
     </main>
   );
 }
